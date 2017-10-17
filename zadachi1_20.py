f112e25087756f45cedeaef324fc7048d9e339d3
AnastasiyaKazachenka <anastasiya.kazachenka@mail.ru>
2017-10-17 14:37:50 +0100

    Update zadachi1_20.py
    added zadacha18_same as zadacha67

--- zadachi1_20.py
+++ zadachi1_20.py
@@ -340,7 +340,44 @@ for x in range(1,10):
 final = sum_1_100+sum_100_999+dict_20_1000[1000]+dict_1_9[1]
 
 
+#zadacha18
+num = """75
+95 64
+17 47 82
+18 35 87 10
+20 04 82 47 65
+19 01 23 75 03 34
+88 02 77 73 07 63 67
+99 65 04 28 06 16 70 92
+41 41 26 56 83 40 80 70 33
+41 48 72 33 47 32 37 16 94 29
+53 71 44 65 25 43 91 52 97 51 14
+70 11 33 28 77 73 17 78 39 68 17 57
+91 71 52 38 17 14 91 43 58 50 27 29 48
+63 66 04 68 89 53 67 30 73 16 69 87 40 31
+04 62 98 27 23 09 70 98 73 93 38 53 60 04 23"""
 
+my_numbers_list = num.split('\n')
+
+list_of_lists = []
+for x in range(0,len(my_numbers_list)) :
+     k = my_numbers_list[x].split( )
+     list_to_add = []
+     for z in range(0, len(k)) :
+         list_to_add.append(int(k[z]))
+     list_of_lists.append(list_to_add)
+         
+list_to_work = list_of_lists[::-1]
+
+for x in range(1, len(list_to_work)):
+    new_row=[]
+    for z in range(0, len(list_to_work[x])):
+        if list_to_work[x-1][z] >= list_to_work[x-1][z+1] :
+            new_number = list_to_work[x][z]+list_to_work[x-1][z]
+        else:
+            new_number = list_to_work[x][z]+list_to_work[x-1][z+1]
+        new_row.append(new_number)
+    list_to_work[x] = new_row
 
 
 

3484461ae13c1067ec28c017d376f0612a5b1dbf
AnastasiyaKazachenka <anastasiya.kazachenka@mail.ru>
2018-10-01 17:05:26 +0100

    Update zadachi_51_60.py
    added53

--- zadachi_51_60.py
+++ zadachi_51_60.py
@@ -75,3 +75,13 @@ while True:
         break
     n=n+1
 
+# zadacha53
+import math
+math.factorial(7)
+
+counter=0
+
+for n in range(1,101):
+    for r in range(1,n+1):
+        if math.factorial(n)/(math.factorial(r)*math.factorial(n-r)) >1000000 :
+            counter=counter+1
